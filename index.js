40b833ecdbe9bd15173bf1a9bb60d65be37b7d63
Hamzahk4 <110351211+Hamzahk4@users.noreply.github.com>
2024-10-18 11:18:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-//STUDENT COUNTER
+
 
 count = 0;
+total=0
 function increment(){
     count++;
     document.getElementById("count-el").innerText=count;
-    
+   
 }
+
 
 function save(){
     nowcount = count;
-    document.getElementById("prev").innerHTML= document.getElementById("prev").innerHTML+"-"+nowcount;
+    total=total+count;
+    document.getElementById("prev").innerHTML= document.getElementById("prev").innerHTML+nowcount+"-";
+    let totals=document.createElement("h3")
+    document.getElementById("tot").innerHTML= "Total: " + count + " Students";
 }
